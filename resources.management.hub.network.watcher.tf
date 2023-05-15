7fd31df97855d97ea309dc60fdf243a8b7ca2b13
John Spinella <jrspinella@users.noreply.github.com>
2023-05-14 20:36:17 -0400

    Updates to FLow Logs

--- resources.management.hub.network.watcher.tf
+++ resources.management.hub.network.watcher.tf
@@ -36,7 +36,7 @@ resource "azurerm_network_watcher_flow_log" "nwflog" {
   resource_group_name       = local.netwatcher_rg_name # Must provide Netwatcher resource Group
   network_security_group_id = azurerm_network_security_group.nsg[each.key].id
   storage_account_id        = module.mgt_sa.storage_account_id
-  enabled                   = var.create_network_watcher != false ? true : false
+  enabled                   = true
   version                   = 2
   retention_policy {
     enabled = true
