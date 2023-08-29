4a183953920f2cc78b4f3d103b9db9879fbb4646
John Spinella <jrspinella@users.noreply.github.com>
2023-08-29 09:47:27 -0400

    Updated to NetworkWatcher

--- data.tf
+++ data.tf
@@ -5,7 +5,12 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_resource_group" "netwatch" {
-  depends_on = [azurerm_virtual_network.hub_vnet]
-  count      = ! var.create_network_watcher ? 1 : 0
+  depends_on = [azurerm_virtual_network.hub_vnet]  
   name       = "NetworkWatcherRG"
+}
+
+data "azurerm_network_watcher" "nwatcher" {
+  depends_on = [ azurerm_virtual_network.hub_vnet ]
+  name                = "NetworkWatcher_${local.location}"  
+  resource_group_name = data.azurerm_resource_group.netwatch.name
 }
--- resources.management.hub.network.watcher.tf
+++ resources.management.hub.network.watcher.tf
@@ -2,32 +2,11 @@
 # Licensed under the MIT License.
 
 /*
-SUMMARY: Terraform Module to deploy the Network Watcher and Flog Logs for Hub Network based on the Azure Mission Landing Zone conceptual architecture
+SUMMARY: Terraform Module to deploy the Network Watcher Flog Logs for Hub Network based on the Azure Mission Landing Zone conceptual architecture
 DESCRIPTION: The following components will be options in this deployment              
               * Network Watcher         
 AUTHOR/S: jspinella
 */
-
-#-------------------------------------
-# Network Watcher - Default is "false", 
-# Network Watcher should only be 
-# created when one is not provided.
-# check README.md for more details 
-#-------------------------------------
-resource "azurerm_resource_group" "nwatcher" {
-  count    = var.create_network_watcher ? 1 : 0
-  name     = "NetworkWatcherRG"
-  location = local.location
-  tags     = merge({ "ResourceName" = "NetworkWatcherRG" }, local.default_tags, var.add_tags, )
-}
-
-resource "azurerm_network_watcher" "nwatcher" {
-  count               = var.create_network_watcher ? 1 : 0
-  name                = "NetworkWatcher_${local.location}"
-  location            = local.location
-  resource_group_name = local.netwatcher_rg_name
-  tags                = merge({ "ResourceName" = format("%s", "NetworkWatcher_${local.location}") }, local.default_tags, var.add_tags, )
-}
 
 #-----------------------------------------
 # Network flow logs for subnet and NSG
@@ -35,8 +14,8 @@ resource "azurerm_network_watcher" "nwatcher" {
 resource "azurerm_network_watcher_flow_log" "nwflog" {
   for_each                  = var.hub_subnets
   name                      = lower("network-watcher-flow-log-${each.value.name}")
-  network_watcher_name      = var.create_network_watcher ? azurerm_network_watcher.nwatcher.0.name : "NetworkWatcher_${local.location}" 
-  resource_group_name       = local.netwatcher_rg_name # Must provide Netwatcher resource Group
+  network_watcher_name      = data.azurerm_network_watcher.nwatcher.name
+  resource_group_name       = data.azurerm_resource_group.netwatch.name # Must provide Netwatcher resource Group
   network_security_group_id = azurerm_network_security_group.nsg[each.key].id
   storage_account_id        = module.mgt_sa.storage_account_id
   enabled                   = true
