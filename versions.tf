efd5020304383d4357ad1b83dc4de35cc45993ea
John Spinella <jrspinella@users.noreply.github.com>
2023-08-29 09:47:37 -0400

    Update Version

--- versions.tf
+++ versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.22"
+      version = "~> 3.36"
     }
     azurenoopsutils = {
       source  = "azurenoops/azurenoopsutils"
