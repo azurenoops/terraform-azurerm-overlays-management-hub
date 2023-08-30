ac09c17806e1a5a055e5bdabae90f0774bdef944
John Spinella <jrspinella@users.noreply.github.com>
2023-08-30 10:25:23 -0400

    Removing not used varaibles

--- variables.subnet.tf
+++ variables.subnet.tf
@@ -5,28 +5,6 @@
 # Subnet Configuration    ##
 ############################
 
-variable "subnet_address_prefix" {
-  description = "The address prefixes to use for the default subnet"
-  type        = list(string)
-  default     = []
-}
-
-variable "subnet_service_endpoints" {
-  description = "Service endpoints to add to the default subnet"
-  type        = list(string)
-  default     = []
-}
-
-variable "private_endpoint_network_policies_enabled" {
-  description = "Whether or not to enable network policies on the private endpoint subnet"
-  default     = null
-}
-
-variable "private_link_service_network_policies_enabled" {
-  description = "Whether or not to enable service endpoints on the private endpoint subnet"
-  default     = null
-}
-
 variable "hub_subnets" {
   description = "A list of subnets to add to the hub"  
   default = {}
