f01b89ccf060375cdf6a4f9c7b80fadd48cd9af3
John Spinella <jrspinella@users.noreply.github.com>
2024-03-28 17:10:48 -0400

    add vnet module

--- data.tf
+++ data.tf
@@ -5,12 +5,12 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_resource_group" "netwatch" {
-  depends_on = [azurerm_virtual_network.hub_vnet]  
+  depends_on = [module.hub_vnet]
   name       = "NetworkWatcherRG"
 }
 
 data "azurerm_network_watcher" "nwatcher" {
-  depends_on = [ azurerm_virtual_network.hub_vnet ]
-  name                = "NetworkWatcher_${local.location}"  
+  depends_on = [ module.hub_vnet ]
+  name                = "NetworkWatcher_${local.location}"
   resource_group_name = data.azurerm_resource_group.netwatch.name
 }
