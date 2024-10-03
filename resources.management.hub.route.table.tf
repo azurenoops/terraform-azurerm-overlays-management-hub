fce4b33a023314d681cf24b1b6fe53cbbeca673a
John Spinella <jrspinella@users.noreply.github.com>
2024-10-03 10:05:58 -0400

    update routetable file

--- resources.management.hub.route.table.tf
+++ resources.management.hub.route.table.tf
@@ -10,7 +10,7 @@ DESCRIPTION: The following components will be options in this deployment
 AUTHOR/S: jrspinella
 */
 
-module "hub_route_table" {
+/* module "hub_route_table" {
   source  = "azure/avm-res-network-routetable/azurerm"
   version = "0.3.0"
 
@@ -54,6 +54,71 @@ module "hub_route_table" {
 
   # tags
   tags = merge({ "ResourceName" = format("%s", local.ddos_plan_name) }, local.default_tags, var.add_tags, )
+}
+
+# Encrypted Transport Route Table
+resource "azurerm_route_table" "afw_routetable" {
+  name                          = local.hub_afw_rt_name
+  resource_group_name           = local.resource_group_name
+  location                      = local.location
+  bgp_route_propagation_enabled = false
+  tags                          = merge({ "ResourceName" = "afw-subnet-route-network-outbound" }, local.default_tags, var.add_tags, )
+
+  count = var.enable_encrypted_transport ? 1 : 0
+}
+
+resource "azurerm_subnet_route_table_association" "afw_rtassoc" {
+  subnet_id      = azurerm_subnet.firewall_client_snet[0].id
+  route_table_id = azurerm_route_table.afw_routetable[0].id
+
+  count = var.enable_encrypted_transport ? 1 : 0
+}
+
+resource "azurerm_route" "afw_route" {
+  name                   = lower(format("route-to-afw-subnet-%s", local.location))
+  resource_group_name    = local.resource_group_name
+  route_table_name       = azurerm_route_table.afw_routetable[0].name
+  address_prefix         = var.encrypted_transport_address_prefix
+  next_hop_type          = var.encrypted_transport_next_hop_type
+  next_hop_in_ip_address = var.encrypted_transport_next_hop_in_ip_address
+
+  count = var.enable_encrypted_transport ? 1 : 0
+} */
+
+
+resource "azurerm_route_table" "routetable" {
+  name                          = local.hub_rt_name
+  resource_group_name           = local.resource_group_name
+  location                      = local.location
+  bgp_route_propagation_enabled = var.disable_bgp_route_propagation
+  tags                          = merge({ "ResourceName" = "route-network-outbound" }, local.default_tags, var.add_tags, )
+}
+
+resource "azurerm_subnet_route_table_association" "rtassoc" {
+  for_each       = var.hub_subnets
+  subnet_id      = azurerm_subnet.default_snet[each.key].id
+  route_table_id = azurerm_route_table.routetable.id
+}
+
+resource "azurerm_route" "force_internet_tunneling" {
+  name                   = lower(format("route-to-firewall-%s", local.hub_vnet_name))
+  resource_group_name    = local.resource_group_name
+  route_table_name       = azurerm_route_table.routetable.name
+  address_prefix         = "0.0.0.0/0"
+  next_hop_in_ip_address = azurerm_firewall.fw[0].ip_configuration[0].private_ip_address
+  next_hop_type          = "VirtualAppliance"
+
+  count = var.enable_firewall && var.enable_forced_tunneling ? 1 : 0
+}
+
+resource "azurerm_route" "route" {
+  for_each               = var.route_table_routes
+  name                   = lower(format("route-to-firewall-%s", each.value.route_name))
+  resource_group_name    = local.resource_group_name
+  route_table_name       = azurerm_route_table.routetable.name
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
+  next_hop_in_ip_address = each.value.next_hop_in_ip_address
 }
 
 # Encrypted Transport Route Table
