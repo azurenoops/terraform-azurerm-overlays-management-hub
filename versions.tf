c13c7bb29d3fa9c46aacafc394022c1dfe9c03ff
John Spinella <jrspinella@users.noreply.github.com>
2024-10-23 13:32:58 -0400

    Emergency Fix for Version # Describe your changes
    - Updated AzureRM version to >= 3.7.0, < 4.0 to give it a bigger scope for backward capability.
    
    Thanks for your cooperation!

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.3"
+      version = ">= 3.7.0, < 4.0"
     }
     azurenoopsutils = {
       source  = "azurenoops/azurenoopsutils"
